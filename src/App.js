4d24292a8fdd6a7ead8684d28437f573109a9ab2
raj-dev143 <rajendra.meena.dev@gmail.com>
2024-04-13 15:40:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,11 +8,29 @@ import {
   Button,
   Avatar,
   CircularProgress,
+  Tooltip,
+  IconButton,
+  Box,
+  Menu,
+  MenuItem,
+  Divider,
+  ListItemIcon,
 } from "@mui/material";
 import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 
+import Logout from "@mui/icons-material/Logout";
+
 function App() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   const { user, loginWithRedirect, isAuthenticated, isLoading, logout } =
     useAuth0();
 
@@ -50,7 +68,7 @@ function App() {
             >
               SS Cricket
             </Typography>
-            <div style={{ display: "flex", alignItems: "center" }}>
+            {/* <div style={{ display: "flex", alignItems: "center" }}>
               {isAuthenticated && (
                 <div
                   style={{
@@ -66,21 +84,86 @@ function App() {
                   </span>
                 </div>
               )}
-              {isAuthenticated ? (
-                <Button
-                  variant="outlined"
-                  size="small"
-                  color="inherit"
-                  onClick={logout}
+            </div> */}
+
+            {isAuthenticated ? (
+              <div>
+                <Box
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    textAlign: "center",
+                  }}
                 >
-                  Logout
-                </Button>
-              ) : (
-                <Button color="inherit" onClick={loginWithRedirect}>
-                  Log In
-                </Button>
-              )}
-            </div>
+                  <Tooltip title="Account settings">
+                    <IconButton
+                      onClick={handleClick}
+                      size="small"
+                      sx={{ ml: 2 }}
+                      aria-controls={open ? "account-menu" : undefined}
+                      aria-haspopup="true"
+                      aria-expanded={open ? "true" : undefined}
+                    >
+                      <Avatar
+                        sx={{ width: 32, height: 32 }}
+                        alt={user.name}
+                        src={user.picture}
+                      />
+                    </IconButton>
+                  </Tooltip>
+                </Box>
+                <Menu
+                  anchorEl={anchorEl}
+                  id="account-menu"
+                  open={open}
+                  onClose={handleClose}
+                  onClick={handleClose}
+                  PaperProps={{
+                    elevation: 0,
+                    sx: {
+                      overflow: "visible",
+                      filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                      mt: 1.5,
+                      "& .MuiAvatar-root": {
+                        width: 32,
+                        height: 32,
+                        ml: -0.5,
+                        mr: 1,
+                      },
+                      "&::before": {
+                        content: '""',
+                        display: "block",
+                        position: "absolute",
+                        top: 0,
+                        right: 14,
+                        width: 10,
+                        height: 10,
+                        bgcolor: "background.paper",
+                        transform: "translateY(-50%) rotate(45deg)",
+                        zIndex: 0,
+                      },
+                    },
+                  }}
+                  transformOrigin={{ horizontal: "right", vertical: "top" }}
+                  anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+                >
+                  <MenuItem>
+                    <Avatar /> {user.name}
+                  </MenuItem>
+                  <Divider />
+                  <MenuItem onClick={logout}>
+                    <ListItemIcon>
+                      <Logout fontSize="small" />
+                    </ListItemIcon>
+                    Logout
+                  </MenuItem>
+                </Menu>
+              </div>
+            ) : (
+              <Button color="inherit" onClick={loginWithRedirect}>
+                Log In
+              </Button>
+            )}
           </Toolbar>
         </AppBar>
         {!isAuthenticated && (
